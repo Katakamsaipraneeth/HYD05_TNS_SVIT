65594c76543ed96954f044757e4f997f77a64581
k sai praneeth <k sai praneeth@LAPTOP-RSB7MO7G>
2023-09-11 12:10:06 +0530

    commit

---
+++ TNS_15/src/com/tnsif/daytwo/IncrementOperator.java
@@ -0,0 +1,15 @@
+package com.tnsif.daytwo;
+
+public class IncrementOperator {
+	public static void main(String[] args) {
+		int x = 10;
+                System.out.println(x++); //postfix
+		int y = 10;
+                System.out.println(++x); //prefix
+                int z = 10;
+                System.out.println(z--);//postfix
+                int a = 10;
+                System.out.println(--a);//prefix
+	}
+
+}
